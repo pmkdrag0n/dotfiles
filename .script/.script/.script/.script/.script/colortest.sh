daf6910c2d3a9d72ee7edb68f8e45c282f521347
minh khoi <pmk.drag0n.1998@gmail.com>
2020-04-08 02:19:00 +0700

    Backup at Wed 08 Apr 2020 02:19:00 AM +07

---
+++ .script/.script/.script/.script/.script/colortest.sh
@@ -0,0 +1,6 @@
+ 
+#!/usr/bin/env bash
+
+# small colortest script, credits go to https://github.com/stark/Color-Scripts
+pcs() { for i in {0..7}; do echo -en "\e[${1}$((30+$i))m \u2588\u2588 \e[0m"; done; }
+printf "\n%s\n%s\n\n" "$(pcs)" "$(pcs '1;')"
